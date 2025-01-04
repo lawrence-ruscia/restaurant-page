1f03b58e11d60d2b2a849075f51d1625ce086eb8
Renz <140231442+lawrnzzz@users.noreply.github.com.>
2025-01-04 21:51:36 +0800

    chore: add html-laoder for inline-html image elements

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,10 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.html$/i,
+        use: ["html-loader"],
+      },
     ],
   },
   plugins: [new HtmlWebpackPlugin({ template: "./src/template.html" })],
