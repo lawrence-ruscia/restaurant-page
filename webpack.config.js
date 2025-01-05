2472a90ccf0951023142800d5dcac0636c083214
Renz <140231442+lawrnzzz@users.noreply.github.com.>
2025-01-05 16:45:40 +0800

    style: add font-family

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,10 @@ module.exports = {
         test: /\.html$/i,
         use: ["html-loader"],
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
     ],
   },
   plugins: [new HtmlWebpackPlugin({ template: "./src/template.html" })],
