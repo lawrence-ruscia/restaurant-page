62911c0dffe7a0bcaf635affd9ab9ccd2fe47927
Renz <140231442+lawrnzzz@users.noreply.github.com.>
2025-01-13 15:23:58 +0800

    feat: create RenderHandler class that will handle all page rendering

--- src/index.js
+++ src/index.js
@@ -3,6 +3,45 @@ import "./normalize.css";
 
 import { HomePage } from "./home.js";
 
-// TEMP: This is only to test the rendering of the homepage
-const content = document.querySelector("#content");
-content.append(new HomePage().render());
+class RenderHandler {
+  #content = document.querySelector("#content");
+
+  #PageSections = {
+    home: new HomePage().render(),
+  };
+
+  constructor() {
+    const home = this.#PageSections.home;
+    this.#appendPage(home); // Initial page
+
+    this.setupEventListener("home", this.#PageSections.home);
+  }
+
+  setupEventListener() {
+    const header = document.querySelector("#header");
+
+    header.addEventListener("click", (e) => {
+      const button = e.target;
+
+      if (button.dataset.page) {
+        const pageKey = button.dataset.page;
+        const page = this.#PageSections[pageKey];
+
+        if (page) {
+          this.#clearPage();
+          this.#appendPage(page);
+        }
+      }
+    });
+  }
+
+  #appendPage(page) {
+    this.#content.append(page);
+  }
+
+  #clearPage() {
+    this.#content.innerHTML = "";
+  }
+}
+
+new RenderHandler();
