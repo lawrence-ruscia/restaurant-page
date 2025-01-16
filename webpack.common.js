c922ea56dd29a8ee76221fcc1b5383c84815f945
Renz <140231442+lawrnzzz@users.noreply.github.com.>
2025-01-17 06:11:43 +0800

    chore: split webpack modes to different files, then add webpack-merge to merge with common configurations

--- webpack.common.js
+++ webpack.common.js
@@ -1,19 +1,14 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { watchFile } = require("fs");
 
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
-  },
+
   module: {
     rules: [
       {
